cadba0b677c45c2bbbcb55258092bef426767bd3
Daniel Caiafa <dcaiafa@istreamplanet.com>
2021-03-14 11:46:15 -0700

    use sync.Pool to reuse lexer/parser

--- internal/parser2/parser.go
+++ internal/parser2/parser.go
@@ -1,17 +1,33 @@
 package parser2
 
 import (
+	"sync"
+
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/dcaiafa/nitro/internal/ast"
 	"github.com/dcaiafa/nitro/internal/errlogger"
 	"github.com/dcaiafa/nitro/internal/parser2/parser"
 )
 
+var parserPool = sync.Pool{
+	New: func() interface{} {
+		return parser.NewNitroParser(nil)
+	},
+}
+
+var lexerPool = sync.Pool{
+	New: func() interface{} {
+		return newAugmentedLexer(nil)
+	},
+}
+
 func Parse(filename string, input string, diagMode bool, errLogger errlogger.ErrLogger) (*ast.Module, error) {
-	inputStream := antlr.NewInputStream(string(input))
-	lexer := newAugmentedLexer(inputStream)
-	nitroParser := parser.NewNitroParser(
-		antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel))
+	lexer := lexerPool.Get().(*augmentedLexer)
+	lexer.SetInputStream(antlr.NewInputStream(string(input)))
+
+	nitroParser := parserPool.Get().(*parser.NitroParser)
+	nitroParser.SetInputStream(antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel))
+
 	nitroParser.RemoveErrorListeners()
 	errListener := newErrorListener(filename, errLogger)
 	nitroParser.AddErrorListener(errListener)
@@ -21,6 +37,8 @@ func Parse(filename string, input string, diagMode bool, errLogger errlogger.Err
 	}
 	listener := newListener(filename)
 	parseTree := nitroParser.Start()
+	parserPool.Put(nitroParser)
+	lexerPool.Put(lexer)
 	if errListener.Error() != nil {
 		return nil, errListener.Error()
 	}
