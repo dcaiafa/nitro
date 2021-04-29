e4cd714860e63619830c61a36fdc88d5381d0bab
Daniel Caiafa <1450558+dcaiafa@users.noreply.github.com>
2021-04-28 18:15:29 -0700

    lines: add option to specify max line size

--- lib/strings.go
+++ lib/strings.go
@@ -188,8 +188,14 @@ func split(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
 	return []nitro.Value{a}, nil
 }
 
+type linesOptions struct {
+	MaxLineSize int64 `nitro:"maxlinesize"`
+}
+
+var linesOptionsConv Value2Structer
+
 func lines(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
-	if len(args) < 1 {
+	if len(args) != 1 && len(args) != 2 {
 		return nil, errNotEnoughArgs
 	}
 	input, err := ToReader(m, args[0])
@@ -197,9 +203,25 @@ func lines(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
 		return nil, fmt.Errorf("invalid argument #1: %w", err)
 	}
 
+	var opt linesOptions
+	if len(args) == 2 {
+		optv, ok := args[1].(*nitro.Object)
+		if !ok {
+			return nil, fmt.Errorf("invalid options arg")
+		}
+		err := linesOptionsConv.Convert(optv, &opt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	l := &linesIter{
 		input:   input,
 		scanner: bufio.NewScanner(input),
+	}
+
+	if opt.MaxLineSize != 0 {
+		l.scanner.Buffer(nil, int(opt.MaxLineSize))
 	}
 
 	outIter := nitro.NewIterator(l.Next, 1)
