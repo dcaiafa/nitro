f6992595f10468523db330ac739d1d24d718ec22
Daniel Caiafa <1450558+dcaiafa@users.noreply.github.com>
2021-05-13 12:15:13 -0700

    printtable

--- lib/print.go
+++ lib/print.go
@@ -1,8 +1,11 @@
 package lib
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"os"
+	"text/tabwriter"
 
 	"github.com/dcaiafa/nitro"
 )
@@ -86,6 +89,138 @@ func sprintf(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
 	res := fmt.Sprintf(format, iargs...)
 
 	return []nitro.Value{nitro.NewString(res)}, nil
+}
+
+type printTableOptions struct {
+	AlignRight bool  `nitro:"alignright"`
+	MinWidth   int64 `nitro:"minwidth"`
+	Padding    int64 `nitro:"padding"`
+	PadChar    int64 `nitro:"padchar"`
+}
+
+var printTableConv Value2Structer
+
+var errPrintTableUsage = errors.New(
+	`invalid usage. Expected printtable(iter, map?)`)
+
+func printtable(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
+	if len(args) != 1 && len(args) != 2 {
+		return nil, errPrintTableUsage
+	}
+
+	inIter, err := nitro.MakeIterator(m, args[0])
+	if err != nil {
+		return nil, errPrintTableUsage
+	}
+
+	opts := printTableOptions{
+		Padding: -1,
+	}
+
+	if len(args) == 2 {
+		err = printTableConv.Convert(args[1], &opts)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var flags uint
+	if opts.AlignRight {
+		flags = flags | tabwriter.AlignRight
+	}
+
+	if opts.Padding == -1 {
+		opts.Padding = 1
+	}
+	if opts.PadChar == 0 {
+		opts.PadChar = ' '
+	}
+
+	tabw := tabwriter.NewWriter(
+		Stdout(m),
+		int(opts.MinWidth),
+		0, /*tabwidth*/
+		int(opts.Padding),
+		byte(opts.PadChar),
+		flags)
+
+	defer tabw.Flush()
+
+	buf := bytes.Buffer{}
+	writeRecord := func(vs []nitro.Value) error {
+		buf.Reset()
+		for _, v := range vs {
+			// TODO: replace \t in value.
+			buf.WriteString(v.String())
+			buf.WriteByte('\t')
+		}
+		buf.WriteByte('\n')
+		_, err := tabw.Write(buf.Bytes())
+		return err
+	}
+
+	first := true
+	var headers []nitro.Value
+	var values []nitro.Value
+	for {
+		vs, ok, err := nitro.Next(m, inIter, 1)
+		if err != nil {
+			return nil, err
+		}
+		if !ok {
+			break
+		}
+
+		rec := vs[0]
+
+		if mrec, ok := rec.(*nitro.Object); ok {
+			if first {
+				headers = make([]nitro.Value, 0, mrec.Len())
+				values = make([]nitro.Value, mrec.Len())
+				err = nil
+				mrec.ForEach(func(k, v nitro.Value) bool {
+					kstr, ok := k.(nitro.String)
+					if !ok {
+						err = fmt.Errorf(
+							"only string keys are supported, but map has key type %v",
+							nitro.TypeName(k))
+						return false
+					}
+					headers = append(headers, kstr)
+					return true
+				})
+				writeRecord(headers)
+			}
+			for i, k := range headers {
+				v, ok := mrec.Get(k)
+				if ok {
+					if vstr, ok := v.(nitro.String); ok {
+						values[i] = vstr
+					} else {
+						values[i] = nitro.NewString(v.String())
+					}
+				} else {
+					values[i] = nitro.NewString("")
+				}
+			}
+			writeRecord(values)
+		} else if _, ok := rec.(*nitro.Array); ok {
+			panic("not impl")
+		} else {
+			return nil, fmt.Errorf(
+				"expected iterator values of array or map; received %v",
+				nitro.TypeName(rec))
+		}
+
+		first = false
+	}
+
+	err = tabw.Flush()
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
 
 func valuesToInterface(values []nitro.Value) []interface{} {
