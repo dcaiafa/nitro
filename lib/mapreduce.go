5672ad7d22f0945305bf70b0e232a272e9193ad3
Daniel Caiafa <1450558+dcaiafa@users.noreply.github.com>
2021-06-01 12:15:58 -0700

    mapreduce: multiple reducers

--- lib/mapreduce.go
+++ lib/mapreduce.go
@@ -8,10 +8,10 @@ import (
 )
 
 var errMapReduceUsage = errors.New(
-	`invalid usage. Expected mapreduce(iter, string|func, string|func, func)`)
+	`invalid usage. Expected mapreduce(iter, string|func, string|func, func+)`)
 
 func mapreduce(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
-	if len(args) != 4 {
+	if len(args) < 4 {
 		return nil, errMapReduceUsage
 	}
 
@@ -31,9 +31,13 @@ func mapreduce(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error)
 		return nil, fmt.Errorf("invalid argument 3: %w", err)
 	}
 
-	reduceFn, ok := args[3].(nitro.Callable)
-	if !ok {
-		return nil, errMapReduceUsage
+	reduceFns := make([]nitro.Callable, 0, len(args)-3)
+	for i := 3; i < len(args); i++ {
+		reduceFn, ok := args[i].(nitro.Callable)
+		if !ok {
+			return nil, errMapReduceUsage
+		}
+		reduceFns = append(reduceFns, reduceFn)
 	}
 
 	res := nitro.NewObject()
@@ -62,24 +66,39 @@ func mapreduce(m *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error)
 			continue
 		}
 
+		var accumList *nitro.Array
 		accumRef, _ := res.IndexRef(mapKey)
-		accumRes, err := m.Call(
-			reduceFn, []nitro.Value{*accumRef.Refo(), val}, 1)
-		if err != nil {
-			return nil, err
+		if *accumRef.Refo() == nil {
+			accumList = nitro.NewArrayFromSlice(make([]nitro.Value, len(reduceFns)))
+			*accumRef.Refo() = accumList
+		} else {
+			accumList = (*accumRef.Refo()).(*nitro.Array)
 		}
 
-		*accumRef.Refo() = accumRes[0]
+		for i, reduceFn := range reduceFns {
+			partialRes, err := m.Call(
+				reduceFn, []nitro.Value{accumList.Get(i), val}, 1)
+			if err != nil {
+				return nil, err
+			}
+			accumList.Put(i, partialRes[0])
+		}
 	}
 
 	err = nil
 	res.ForEach(func(k, accum nitro.Value) bool {
-		accumRes, callErr := m.Call(reduceFn, []nitro.Value{accum}, 1)
-		if callErr != nil {
-			err = callErr
-			return false
+		accumList := accum.(*nitro.Array)
+		for i, reduceFn := range reduceFns {
+			finalRes, callErr := m.Call(reduceFn, []nitro.Value{accumList.Get(i)}, 1)
+			if callErr != nil {
+				err = callErr
+				return false
+			}
+			accumList.Put(i, finalRes[0])
 		}
-		res.Put(k, accumRes[0])
+		if len(reduceFns) == 1 {
+			res.Put(k, accumList.Get(0))
+		}
 		return true
 	})
 
