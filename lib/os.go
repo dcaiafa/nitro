d5ed7774fb4c270e55713b9d80dc89a6784c961f
Daniel Caiafa <dcaiafa@istreamplanet.com>
2022-08-22 09:54:38 -0700

    os package

---
+++ lib/os.go
@@ -0,0 +1,29 @@
+package lib
+
+import (
+	"os"
+
+	"github.com/dcaiafa/nitro"
+)
+
+func osUserHomeDir(vm *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
+	if err := expectArgCount(args, 0, 0); err != nil {
+		return nil, err
+	}
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	return []nitro.Value{nitro.NewString(dir)}, nil
+}
+
+func osWD(vm *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
+	if err := expectArgCount(args, 0, 0); err != nil {
+		return nil, err
+	}
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	return []nitro.Value{nitro.NewString(dir)}, nil
+}
--- lib/user_home_dir.go
+++ lib/user_home_dir.go
@@ -1,18 +1,1 @@
 package lib
-
-import (
-	"os"
-
-	"github.com/dcaiafa/nitro"
-)
-
-func homeDir(vm *nitro.VM, args []nitro.Value, nRet int) ([]nitro.Value, error) {
-	if err := expectArgCount(args, 0, 0); err != nil {
-		return nil, err
-	}
-	dir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
-	return []nitro.Value{nitro.NewString(dir)}, nil
-}
